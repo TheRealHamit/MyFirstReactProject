06d8b5df5024a25e8adfa6ad0650ff441924b23c
Spencer Leyh <spencerleyh@gmail.com>
2024-01-18 19:04:36 -0800

    Added featured pup functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
   return (
     <>
     <div>
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
+      <p>{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</p>
     </div>
     </>
   )
